f20366ca85b3fbf2b946e1b08baf95151a819f71
Heng Li <chuahengli@gmail.com>
2026-01-04 22:21:37 +0800

    Completed the moomoo api feature of the code, interacting with api and defining variables

--- moomoo_api.py
+++ moomoo_api.py
@@ -4,14 +4,7 @@ import subprocess
 from dotenv import load_dotenv
 import moomoo as moomoo
 from moomoo.trade.open_trade_context import OpenSecTradeContext
-import pandas as pd
-import yfinance as yf
 from datetime import datetime
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
-import re
-import yfinance as yf
 from typing import Optional, Dict, List
 
 
@@ -31,13 +24,71 @@ def start_opend_headless(opend_path):
         print(f"Error: OpenD executable not found at {opend_path}")
         return None
     
+def account_list(trade_obj: OpenSecTradeContext):
+    ret, data = trade_obj.get_acc_list()
+    if ret == moomoo.RET_OK:
+        return data
+    else:
+        raise Exception('get_acc_list error: ', data)
+    
+def account_info(trade_obj: OpenSecTradeContext):
+    ret, data = trade_obj.accinfo_query(trd_env="REAL",refresh_cache=True,currency="SGD")
+    if ret == moomoo.RET_OK:
+        return data
+    else:
+        raise Exception('accinfo_query error: ', data)
+    
+def get_positions(trade_obj: OpenSecTradeContext):
+    ret, data = trade_obj.position_list_query(trd_env="REAL",refresh_cache=True)
+    if ret == moomoo.RET_OK:
+        return data
+    else:
+        raise Exception('position_list_query error: ', data)
+    
+def account_cashflow(trade_obj: OpenSecTradeContext, date:str):
+    ret, data = trade_obj.get_acc_cash_flow(clearing_date=date,trd_env="REAL")
+    if ret == moomoo.RET_OK:
+        return data
+    else:
+        raise Exception('get_acc_cash_flow error: ', data)
+    
+def get_historical_orders(trade_obj: OpenSecTradeContext):
+    ret, data = trade_obj.history_order_list_query()
+    if ret == moomoo.RET_OK:
+        return data
+    else:
+        raise Exception('history_order_list_query error: ', data)
+    
     
 def main():
     opend_path = r"moomoo_OpenD_9.6.5618_Windows\OpenD.exe"
     opend_process = start_opend_headless(opend_path)
-    time.sleep(5)
+    
+    # Get the RSA key path from environment variables
+    load_dotenv()
+    key_path = os.getenv("KEY_PATH")
+    # 1. Configure the RSA private key file globally
+    moomoo.SysConfig.set_init_rsa_file(key_path)
+    # 2. Create the trade context and enable encryption
+    # is_encrypt=True encrypts using RSA key above
+    trade_ctx = OpenSecTradeContext(
+        host='127.0.0.1',
+        port=11111,
+        is_encrypt=True,
+        security_firm="FUTUSG"
+        )
+    data = {
+        "account_list": account_list(trade_ctx),
+        "account_info": account_info(trade_ctx),
+        "positions": get_positions(trade_ctx),
+        "cashflow": account_cashflow(trade_ctx, date=datetime.now().strftime('%Y-%m-%d')),
+        "historical_orders": get_historical_orders(trade_ctx)
+        }
+    
+    trade_ctx.close()
     opend_process.terminate()
-    return
+    
+    return data
 
 if __name__ == "__main__":
     main()
